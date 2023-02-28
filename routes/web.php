cd8b1f04ac4fb69c9a9a99b6021a3a5473525aeb
jogonzalez90 <jogo990@hotmail.com>
2023-02-27 21:50:17 -0500

    Clase II

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    //Consulta a base de datos
+
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
+});
+
+
+
+
